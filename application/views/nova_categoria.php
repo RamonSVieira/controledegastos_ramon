9154a91405d7ac7b474316fe54416455df7d6a70
Sn4k3rx <ramonvieira122333@gmail.com>
2021-02-23 16:20:52 -0300

    conclusão da atividade

---
+++ application/views/nova_categoria.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <label for="">
+            Nome:
+            <input type="text" name="nome" id="">
+        </label>
+
+        <label for="">
+            Descrição:
+            <input type="text" name="descricao" id="">
+        </label>
+
+        <input type="submit" value="ENVIAR">
+        <input type="reset" value="LIMPAR">
+    </form>
+</body>
+
+</html>
---
+++ application/views/novo_gasto.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+
+        <label>
+            Data:
+            <input type="date" name="data">
+        </label>
+
+        <label>
+            Descrição:
+            <input type="text" name="descricao">
+        </label>
+
+        <label>
+            Valor:
+            <input type="number" name="valor">
+        </label>
+
+        <input type="submit" value="ENVIAR">
+        <input type="reset" value="LIMPAR">
+    </form>
+</body>
+
+</html>
